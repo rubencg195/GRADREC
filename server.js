3672d9c6d607b365265b59b63cdbe2384a683b00
Ruben Chevez <rubencg195@hotmail.com>
2018-09-28 01:19:19 -0230

    EJS + Express Installation

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app     = express();
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
 app.listen(3000, function() {
     console.log("Listening on 3000");
 });
 
-app.get('/', function(request, response){
-    response.send('Hello World');
+app.get('/', function(req, res){
+    //response.sendFile('pages/home/index.html' , { root : __dirname});
+    res.render('pages/home');
 });
 
 app.get('/login', function(request, response){
